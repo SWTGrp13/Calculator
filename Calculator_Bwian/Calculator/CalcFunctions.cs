56e6f785fe3461a037e734bdfb89b618687c78e7
Bwian <au590540@uni.au.dk>
2019-01-30 11:36:42 +0100

    Brians lommeregner

---
+++ Calculator_Bwian/Calculator/CalcFunctions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    class CalcFunctions
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            var sum = a - b;
+            return sum;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            var sum = a * b;
+            return sum;
+        }
+
+        public double Power(double x, double exp)
+        {
+            var sum = Math.Pow(x, exp);
+            return sum;
+        }
+    }
+}
---
+++ Calculator_Bwian/Calculator/Funcs.cs
@@ -0,0 +1,31 @@
+﻿using System;
+//using static System.Math;
+
+namespace Calculator
+{
+    public class Funcs
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            var sum = a - b;
+            return sum;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            var sum = a * b;
+            return sum;
+        }
+
+        public double Power(double x, double exp)
+        {
+            var sum = Math.Pow(x, exp);
+            return sum;
+        }
+    }
+}
