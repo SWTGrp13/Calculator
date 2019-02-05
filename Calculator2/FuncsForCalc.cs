024dd06617bf8573946726b20bbb10661058ceac
Bwian <au590540@uni.au.dk>
2019-02-05 12:08:43 +0100

    test2

---
+++ Calculator2/FuncsForCalc.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator2
+{
+    public class FuncsForCalc
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return Math.Pow(x, exp);
+        }
+
+        public double Division(double a, double b)
+        {
+            try
+            {
+                if (b == 0)
+                    throw new System.DivideByZeroException();
+                return a / b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("NOOO! THE WORLD WILL IMPLOOOOOODDDDEEEE!!!");
+                throw;
+            }
+        }
+    }
+}
---
+++ Calculator2/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Linq;
+
+namespace Calculator2
+{
+    class Program
+    {
+        private static readonly string[] Operations = {"+", "-", "*", "/", "^", "%"};
+
+        static void Main(string[] args)
+        {
+            double result = 0;
+            double firstNumber = SetNumber("Type you first number : ");
+            double secondNumber = SetNumber("Type you second number: ");
+
+            string stringOperation = SetOperation("Enter the operation + (Add), - (Subtract), * (Multiply) or ^ (Power):");
+
+            switch (stringOperation)
+            {
+                case "+":
+                case "Add":
+                    result = firstNumber + secondNumber;
+                    break;
+                case "-":
+                case "Subtract":
+                    result = firstNumber - secondNumber;
+                    break;
+                case "*":
+                case "Multiply":
+                    result = firstNumber * secondNumber;
+                    break;
+               // case "/":
+               // case "division":
+               //     result = firstNumber / secondNumber;
+               //     break;
+                case "^":
+                case "Power":
+                    result = Math.Pow(firstNumber, secondNumber);
+                    break;
+            }
+
+            Console.WriteLine("Result of {0} {1} {2} = {3}", firstNumber, stringOperation, secondNumber, result);
+            Console.ReadKey();
+        }
+
+        private static double SetNumber(string outputText)
+        {
+            double parse;
+            Console.Write(outputText);
+            string tempInput = Console.ReadLine();
+            while (!double.TryParse(tempInput, out parse))
+            {
+                Console.WriteLine("Incorrect input !");
+                Console.Write(outputText);
+                tempInput = Console.ReadLine();
+            }
+            return double.Parse(tempInput);
+        }
+
+        private static bool IsValidOperation(string input)
+        {
+            return Operations.Contains(input);
+        }
+
+        private static string SetOperation(string outputText)
+        {
+            Console.Write(outputText);
+            string tempInput = Console.ReadLine();
+            while (!IsValidOperation(tempInput))
+            {
+                Console.WriteLine("Incorrect input !");
+                Console.Write(outputText);
+                tempInput = Console.ReadLine();
+            }
+
+            return tempInput;
+        }
+    }
+}
